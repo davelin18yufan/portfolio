6e94a40aeecd8583900a209268db8bfad0df99ee
davelin18yufan <davelin30630@gmail.com>
2024-05-20 21:18:08 +0800

    [feat] project init and add home, projects page

---
+++ components/Grid.tsx
@@ -0,0 +1,37 @@
+import { gridItems } from "@/data"
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid"
+
+export default function Grid() {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              key={id}
+              {...{
+                id,
+                title,
+                description,
+                img,
+                imgClassName,
+                titleClassName,
+                className,
+                spareImg,
+              }}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  )
+}
